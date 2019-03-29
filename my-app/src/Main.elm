86c787c7575453870127f00712d813f6cecb125c
Pierre Beitz <pierrebeitz@gmail.com>
2019-03-29 09:21:40 +0100

    playing around with elm-graphql

--- my-app/src/Main.elm
+++ my-app/src/Main.elm
@@ -1,8 +1,40 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
+import Graphql.Http
+import Graphql.Operation exposing (RootQuery)
+import Graphql.SelectionSet as SelectionSet exposing (SelectionSet)
 import Html exposing (Html, div, h1, img, node, text)
 import Html.Attributes as HA exposing (src)
+import StarWars.Object
+import StarWars.Object.Human as Human
+import StarWars.Query as Query
+import StarWars.Scalar exposing (Id(..))
+
+
+query : SelectionSet (Maybe Human) RootQuery
+query =
+    Query.human { id = Id "1001" } humanSelection
+
+
+type alias Human =
+    { name : String
+    , homePlanet : Maybe String
+    }
+
+
+humanSelection : SelectionSet Human StarWars.Object.Human
+humanSelection =
+    SelectionSet.map2 Human
+        Human.name
+        Human.homePlanet
+
+
+makeRequest : Cmd Msg
+makeRequest =
+    query
+        |> Graphql.Http.queryRequest "https://elm-graphql.herokuapp.com"
+        |> Graphql.Http.send GotResponse
 
 
 
@@ -10,12 +42,18 @@ import Html.Attributes as HA exposing (src)
 
 
 type alias Model =
-    {}
+    { name : String
+    , homePlanet : Maybe String
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { name = "quatsch"
+      , homePlanet = Just "mit soße"
+      }
+    , makeRequest
+    )
 
 
 
@@ -23,12 +61,28 @@ init =
 
 
 type Msg
-    = NoOp
+    = GotResponse (Result (Graphql.Http.Error (Maybe Human)) (Maybe Human))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        GotResponse (Ok maybeHuman) ->
+            case maybeHuman of
+                Just human ->
+                    ( { model
+                        | name = human.name
+                        , homePlanet =
+                            human.homePlanet
+                      }
+                    , Cmd.none
+                    )
+
+                Nothing ->
+                    ( { name = "", homePlanet = Just "" }, Cmd.none )
+
+        GotResponse (Err maybeHuman) ->
+            ( model, Cmd.none )
 
 
 
@@ -38,13 +92,7 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ Html.node "ag-grid"
-            [ HA.style "height" "600px"
-            , HA.attribute "columnDefs" "[{\"headerName\": \"make\"}]"
-            , HA.attribute "rowData" "[{\"make\": \"Toyota\"}]"
-            ]
-            []
-        ]
+        [ text <| model.name ++ " " ++ Maybe.withDefault "kein planet" model.homePlanet ]
 
 
 
